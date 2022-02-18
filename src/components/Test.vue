df41d6d3f2661fd6c9b945b87ba52db894440d10
novickay <novickaya@sputnikfund.ru>
2022-02-18 17:59:36 +0700

    add custom Charts

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,60 +1,57 @@
 <template>
   <div class="legend">
     <div class="legend-item" v-for="item in data.datasets" :key="item">
-      <div class="legend-item-color" :style="`background: ${item.backgroundColor};`" />
-      <span>{{item.label}}</span>
+      <div
+        class="legend-item-color"
+        :style="`background: ${item.backgroundColor};`"
+      />
+      <span>{{ item.label }}</span>
     </div>
   </div>
 
-  <canvas ref="canvas" ></canvas>
+  <canvas ref="canvas"></canvas>
 </template>
 
 <script>
-import { Chart } from 'chart.js'
+import { Chart } from "chart.js";
 
-import {BarController,} from 'chart.js';
+import { BarController } from "chart.js";
 class Custom extends BarController {
   draw() {
     super.draw(arguments);
 
     const meta = this.getMeta();
 
-    meta.data = meta.data.map(i => {
-      i.options.borderRadius = 10
-      i.options.borderColor = '#fff'
-      i.width = 7
-      return i
-    })
-
-
+    meta.data = meta.data.map((i) => {
+      i.options.borderRadius = 10;
+      i.options.borderColor = "#fff";
+      i.width = 7;
+      return i;
+    });
 
     const pt0 = meta.data[0];
-    const {x, y} = pt0.getProps(['x', 'y']);
-    const {radius} = pt0.options;
+    const { x, y } = pt0.getProps(["x", "y"]);
+    const { radius } = pt0.options;
     const ctx = this.chart.ctx;
 
-
     ctx.save();
-    ctx.fillStyle = '#cd6eee'
-    ctx.strokeStyle = 'red';
+    ctx.fillStyle = "#cd6eee";
+    ctx.strokeStyle = "red";
     ctx.lineWidth = 10;
-    ctx.font = '600 28px Poppins'
+    ctx.font = "600 28px Poppins";
     ctx.strokeRect(x - radius, y - radius, 2 * radius, 2 * radius);
     ctx.restore();
-
   }
 }
 
-
-
 export default {
-  name: 'Test',
+  name: "Test",
   extends: Custom,
   components: {},
   props: {
     chartId: {
       type: String,
-      default: '',
+      default: "",
     },
     backgroundColors: {
       type: Array,
@@ -66,7 +63,7 @@ export default {
     },
     options: {
       type: Object,
-      default: () => {}
+      default: () => {},
     },
     borderWidth: {
       type: Number,
@@ -82,42 +79,82 @@ export default {
     },
   },
   data() {
-    return {
-
-    }
+    return {};
   },
   mounted() {
-     this.callRenderFunction()
-
-
+    this.callRenderFunction();
   },
   methods: {
     callRenderFunction() {
-      Custom.id = 'vertical-grouped';
+      Custom.id = "vertical-grouped";
       Custom.defaults = BarController.defaults;
 
-
-      Chart.defaults.plugins.legend.display = false
+      Chart.defaults.plugins.legend.display = false;
       Chart.register(Custom);
 
-      const ctx = this.$refs.canvas.getContext('2d')
-
+      const ctx = this.$refs.canvas.getContext("2d");
 
       new Chart(ctx, {
-           type: 'vertical-grouped',
-           data: this.data,
-           options: {
+        type: "vertical-grouped",
+        data: this.data,
+        options: {
+          ...this.options,
+          //todo width og 1 category OR bars alignment
+          // categoryPercentage: 0.4,
+          // barPercentage: 1.0,
 
-// ...this.options,
-             //todo width og 1 category OR bars alignment
-             categoryPercentage: 0.4,
-             barPercentage: 1.0,
-           }
-         });
+          scales: {
+            x: {
+              stacked: true,
+              display: true,
+              grid: {
+                display: false,
+                drawBorder: false,
+              },
+              ticks: {
+                fontColor: "#1865c9",
+                // fontFamily: 'Poppins',
+                padding: 4,
+                // padding: 14,
+                color: (context) =>
+                  context.tick && context.tick.major && "#00ff04",
+                font: function (context) {
+                  if (context.tick && context.tick.major) {
+                    return {
+                      weight: "bold",
+                    };
+                  }
+                },
+              },
+            },
+            y: {
+              stacked: true,
+              // padding: 34,
+              grid: {
+                display: true,
+                drawBorder: false,
+                borderDash: [4, 3],
+                zeroLineBorderDash: [4, 3],
+                zeroLineColor: "#E4ECF7",
+                color: "#E4ECF7",
+              },
 
+              ticks: {
+                // fontColor: '#A0AEC0',
+                // fontFamily: 'Poppins',
+                maxTicksLimit: 6,
+                minTicksLimit: 6,
+                min: 0,
+                max: 100,
+                padding: 0,
+              },
+            },
+          },
+        },
+      });
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped lang="scss">
