8019b2bf931d1246c34f14d4865f986b48cf6955
Miroslav Vasilev <mironformir@gmail.com>
2022-02-23 09:07:49 +0700

    fix

--- src/calendar/src/components/Calendar.vue
+++ src/calendar/src/components/Calendar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="calendar">
+  <div class="calendar" :style="getVars">
     <ul class="calendar__controls">
       <li class="calendar__controls-left" @click="changeMonth(-1)">
         <BootstrapIcon icon="chevron-left" />
@@ -37,13 +37,32 @@
 <script lang="ts" setup>
 import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
 
-import { ref, defineEmits } from "vue";
+import { ref, defineEmits, defineProps, computed, toRefs } from "vue";
 
+import { variables, getFont, getFontWeight } from "@/helpers/config";
+
+// Тип для ISOString, формально строка, но для невозможности пробросить просто string сделан этот тип
+// Нужно кастить этот тип к строке ISOString, сделано для более выраженной типизации
 interface ISODateDifferentiator extends String {
   [key: string]: unknown;
 }
 export type ISODate = ISODateDifferentiator & string;
 
+type Props = {
+  modelValue?: ISODate;
+  font?: "Inter" | "Open Sans" | "Raleway";
+  weight?: "medium" | "regular" | "bold";
+  color?: string;
+  activeColor?: string;
+  activeBackgroundColor?: string;
+};
+
+const props = defineProps<Props>();
+
+const { modelValue, font, weight, color, activeColor, activeBackgroundColor } =
+  toRefs(props);
+
+// Функции-помошники, используются в основном в map или filter
 const generateWeekDaysFromIterator = (weekday: Date, i: number): Date =>
   new Date(weekday.setDate(weekday.getDate() - weekday.getDay() + i + 1));
 
@@ -56,10 +75,14 @@ const formatToISODate = (el: Date): ISODate =>
 const isDateInCurMonth = (date: ISODate | Date, curMonth: ISODate | Date) =>
   new Date(date).getMonth() === new Date(curMonth).getMonth();
 
-const emit = defineEmits<{ (e: "selected", payload: ISODate[]): void }>();
+//Эммиты
+const emit =
+  defineEmits<{ (e: "update:modelValue", payload: ISODate[]): void }>();
 
+//Локаль для форматирования даты
 const locale = ref<string>("us");
 
+//Генерация массива дятна месяц
 const generateDates = (curMonth: Date): ISODate[] =>
   Array.from(
     new Set(
@@ -79,6 +102,7 @@ const generateDates = (curMonth: Date): ISODate[] =>
 
 const dates = ref<ISODate[]>(generateDates(new Date()));
 
+//Генерация дней недели
 const weekdays = ref<string[]>(
   new Array(7)
     .fill(new Date())
@@ -91,6 +115,7 @@ const weekdays = ref<string[]>(
     .map((weekday) => capitalize(weekday))
 );
 
+//Определяет, находится ли дата в диапазоне междувыбранных дат
 const isInDateRange = (day: ISODate): boolean => {
   const [min, max] = selectedDays.value
     .map((el) => new Date(el).getTime())
@@ -99,8 +124,10 @@ const isInDateRange = (day: ISODate): boolean => {
   return dayTime >= min && dayTime <= max;
 };
 
+//Определяет, выбрана ли дата
 const isDateSelected = (day: ISODate) => selectedDays.value.includes(day);
 
+//Определяет, находится ли дата над или под выбранной датой, нужно для верстки
 const isBeyondOrOnDateSelected = (
   day: ISODate,
   mode: "beyond" | "on" = "beyond"
@@ -120,6 +147,7 @@ const isBeyondOrOnDateSelected = (
   return (mode === "on" ? min : max).getDay() === new Date(day)?.getDay();
 };
 
+//Дата для текущего месяца
 const curMonth = ref(
   (() => {
     const day = new Date();
@@ -128,6 +156,7 @@ const curMonth = ref(
   })()
 );
 
+//Функция для смены месяца
 const changeMonth = (shift: number): void => {
   curMonth.value.setMonth(curMonth.value.getMonth() + shift);
   curMonth.value = new Date(curMonth.value);
@@ -136,11 +165,13 @@ const changeMonth = (shift: number): void => {
 
 const selectedDays = ref<ISODate[]>([] as ISODate[]);
 
+//Отобрадает только день, даты хранятся в ISODate
 const displayOnlyDay = (dateString: ISODate): string =>
   new Date(dateString).toLocaleString(locale.value, {
     day: "numeric",
   });
 
+//Отобрадает только месяц и год, даты хранятся в ISODate
 const displayOnlyMonth = (dateString: ISODate): string =>
   new Date(dateString).toLocaleString(locale.value, {
     month: "long",
@@ -149,11 +180,12 @@ const displayOnlyMonth = (dateString: ISODate): string =>
 
 let mouseMayEnter = false;
 
+//Выбор даты
 const selectDate = (dateString: ISODate): void => {
   selectedDays.value.length >= 2 && !mouseMayEnter && declineSelect();
   selectedDays.value.push(dateString);
   selectedDays.value = Array.from(new Set(selectedDays.value)).sort();
-  emit("selected", selectedDays.value);
+  emit("update:modelValue", selectedDays.value);
   mouseMayEnter = mouseMayEnter ? false : true;
 };
 
@@ -161,26 +193,42 @@ const declineSelect = (): void => {
   selectedDays.value = [];
 };
 
+//Hover для даты
 const queryHover = (dateString: ISODate): void => {
   if (mouseMayEnter) {
     selectedDays.value[1] = dateString;
     selectedDays.value = [...selectedDays.value];
   }
 };
+
+const getVars = computed(() => ({
+  "--color": color?.value || variables.gray600,
+  "--active-color": activeColor?.value || variables.primaryAccent,
+  "--active-background-color":
+    activeBackgroundColor?.value || variables.accentOpacity1,
+  "--font": getFont(font?.value),
+  "--font-weight": getFontWeight(weight?.value),
+}));
 </script>
 
 <style scoped lang="scss">
+@import "../assets/scss/variables.scss";
+
 .calendar {
   display: flex;
   flex-direction: column;
   width: fit-content;
   padding: 24px 30px;
+  font-family: var(--font);
+  box-shadow: $shadow-2xl;
+  border-radius: 20px;
 
   &__controls {
     display: flex;
     list-style: none;
     justify-content: space-between;
     align-items: center;
+    font-size: $p5-font-size;
     margin: 0;
     padding: 0;
     height: 36px;
@@ -193,6 +241,10 @@ const queryHover = (dateString: ISODate): void => {
       justify-content: center;
       align-items: center;
     }
+  }
+
+  &__weekdays {
+    font-size: $p6-font-size;
   }
 
   &__weekdays,
@@ -210,30 +262,42 @@ const queryHover = (dateString: ISODate): void => {
       align-items: center;
       width: 40px;
       height: 40px;
+      transition: 0.3s;
+      position: relative;
+
+      &::before,
+      &::after {
+        position: absolute;
+        content: "";
+        display: block;
+        background-color: transparent;
+        z-index: 0;
+        width: 12px;
+        height: 100%;
+        transition: 0.2s;
+      }
     }
   }
 
   &__days {
     li {
+      font-size: $p5-font-size;
+
       &.--active {
-        background-color: blue;
+        background-color: var(--active-color);
+        color: white;
         border-radius: 4px;
         z-index: 1;
       }
 
       &.--in-range {
         position: relative;
-        background-color: turquoise;
+        background-color: var(--active-background-color);
+        color: $primary-accent;
 
         &::before,
         &::after {
-          position: absolute;
-          content: "";
-          display: block;
-          background-color: turquoise;
-          z-index: 0;
-          width: 12px;
-          height: 100%;
+          background-color: var(--active-background-color);
         }
 
         &::before {
