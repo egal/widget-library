9532c3c3140d1fbfbd17a7be3ddb0069f7f79ddf
yulya yushkova <yushkova@sputnikfund.ru>
2022-03-05 16:16:03 +0700

    README.md update

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = []
 
 const router = createRouter({
-  history: createWebHistory(import.meta.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
