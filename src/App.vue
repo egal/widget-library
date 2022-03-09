c188a5fe0aee10b609bd3e4db2ff2c5e6feab1d7
yulya yushkova <yushkova@sputnikfund.ru>
2022-03-09 14:04:35 +0700

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  jjkhkh
-  <!--  <div v-for="(item, idx) in jsonData" :key="idx" style="margin-bottom: 40px">-->
-  <Calendar :data="item" @update:dateValue="setDate" />
-  <!--  </div>-->
+  <div v-for="(item, idx) in data" :key="idx" style="margin-bottom: 40px">
+    <Calendar :data="item" @update:dateValue="setDate" />
+  </div>
 </template>
 
 <script lang="ts">
-import Calendar from '@/components/Calendar/Calendar.vue'
+import Calendar from '@/components/calendar/Calendar.vue'
 // import jsonData from '@/data.json'
 import { defineComponent } from 'vue'
 export default defineComponent({
@@ -14,24 +13,51 @@ export default defineComponent({
   components: { Calendar },
   data() {
     return {
-      item: {
-        fontFamily: 'Raleway',
-        fontSize: '14px',
-        fontWeight: 'regular',
-        activeColor: '#0066FF',
-        activeBackgroundColor: '#E5F0FF',
-        isAdaptiveSize: false,
-        isDouble: true,
-        locale: 'ru-RU',
-        timePicker: {
-          isAMPM: true,
-          label: 'time 1',
+      data: [
+        {
+          fontFamily: 'Raleway',
+          fontSize: '14px',
+          fontWeight: 'regular',
+          activeColor: '#0066FF',
+          activeBackgroundColor: '#E5F0FF',
+          isAdaptiveSize: false,
+          isDouble: true,
+          locale: 'ru-RU',
+          timePicker: {
+            isAMPM: true,
+            label: 'time 1',
+          },
+          data: {
+            // date_from: '2022-03-02T04:28:12.089Z',
+            // date_to: '2022-03-05',
+          },
         },
-        data: {
-          date_from: '2022-03-02T04:28:12.089Z',
-          date_to: '2022-03-05',
+        {
+          fontFamily: 'Raleway',
+          fontSize: '14px',
+          fontWeight: 500,
+          activeColor: '#51e',
+          activeBackgroundColor: '#E5F0FF',
+          isAdaptiveSize: false,
+          isDouble: false,
+          locale: 'en-US',
+          timePicker: {
+            isAMPM: false,
+            label: 'time 2',
+          },
+          data: {
+            // date_from: '2022-03-02',
+          },
         },
-      },
+        {
+          fontFamily: 'Open Sans',
+          fontSize: '18px',
+          activeColor: 'red',
+          activeBackgroundColor: '#ffF0FF',
+          isAdaptiveSize: false,
+          data: {},
+        },
+      ],
       // jsonData,
     }
   },
