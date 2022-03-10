9ac6bcb361c66d67c6108aa41eac08d52c2a8486
novickay <novickaya@sputnikfund.ru>
2022-03-10 13:19:50 +0700

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,15 @@
-<template>
-  <div style="width: 228px">
-    <ESelect
-      @update:modelValue="onSelect"
-      :data="{
-        searchable: false,
-        searchableInput: true,
-        size: 'md',
-        options: [
-          {
-            name: 'Option one',
-            key: 1,
-          },
-          {
-            name: 'Option two',
-            key: 2,
-          },
-          {
-            name: 'Option three',
-            key: 3,
-          },
-        ],
-      }"
-    />
-  </div>
-</template>
+<template></template>
 
 <script lang="ts">
-import ESelect from '@/components/inputs/Select/ESelect.vue'
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'App',
-  components: { ESelect },
+  components: {},
   data() {
     return {}
   },
   mounted() {},
-  methods: {
-    onSelect(v) {
-      console.log(v)
-    },
-  },
+  methods: {},
 })
 </script>
 
