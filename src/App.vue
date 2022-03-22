9cfaf94da114d2b8894aed39ee83c70ee914aec7
novickay <novickaya@sputnikfund.ru>
2022-03-22 13:58:23 +0700

    fix: remove warning

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<template></template>
+<template>
+  <div :style="{ width: '300px' }">
+    <EPagination :minimalistic-version="true"></EPagination>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import EPagination from '@/components/navigation/EPagination.vue'
 export default defineComponent({
   name: 'App',
-  components: {},
-  data() {},
+  components: { EPagination },
+  data() {
+    return {}
+  },
   methods: {},
 })
 </script>
