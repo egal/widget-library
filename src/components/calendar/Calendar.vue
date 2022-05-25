94f2ac9a6c03cdf00daf0535a00e9f36135644ca
novickay <novickaya@sputnikfund.ru>
2022-05-25 16:39:09 +0700

    Merge branch 'calendar' into tests_ECalendar
    # Conflicts:
    #	src/components/calendar/components/SelectTime.vue

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -3,11 +3,16 @@
     <div
       :class="`date-inputs date-inputs--${mergedData?.inputData?.size ?? 'sm'} ${
         imitateInputsFocus ? 'focused' : ''
-      } ${mergedData.showInput && mergedData.timePicker && !mergedData.isDouble ? 'doubled' : ''}`"
+      } ${
+        mergedData.showInput && mergedData.timePicker && !mergedData.isDouble && !mergedData.isRange
+          ? 'doubled'
+          : ''
+      }`"
       :style="getInputFocusStyle"
     >
       <EInput
         class="calendar__input left"
+        :class="{ single: !mergedData?.timePicker || mergedData.isRange }"
         :style-config="mergedInputStyles"
         :data="mergedLeftInputData"
         v-if="mergedData.showInput"
@@ -17,10 +22,15 @@
       />
       <EInput
         class="calendar__input right"
-        :class="{ ampm: this?.mergedData?.timePicker?.isAMPM }"
+        :class="{ ampm: mergedData?.timePicker?.isAMPM }"
         :style-config="mergedInputStyles"
         :data="mergedRightInputData"
-        v-if="mergedData.showInput && mergedData.timePicker && !mergedData.isDouble"
+        v-if="
+          mergedData.showInput &&
+          mergedData.timePicker &&
+          !mergedData.isDouble &&
+          !mergedData.isRange
+        "
         @error="(error) => handleInputError(error, 'time')"
         @keyup.enter="(event) => handleModelUpdate(event.target.value, 'time')"
         @mouseup="open"
@@ -49,7 +59,7 @@
         />
 
         <SelectTime
-          v-if="mergedData?.timePicker && !mergedData.isDouble"
+          v-if="mergedData?.timePicker && !mergedData.isDouble && !mergedData.isRange"
           :config="mergedData?.timePicker"
           :hours="getHoursFromTimestamp(leftSelectTime)?.hours"
           :minutes="
@@ -187,7 +197,7 @@ export default defineComponent({
           type: 'text',
           size: 'sm',
           clearable: false,
-          iconLeft: this.leftInputValue ? 'clock' : '',
+          iconLeft: this.rightInputValue ? 'clock' : '',
           readonly: true,
           showFilled: !!this.leftInputValue,
         },
@@ -801,6 +811,12 @@ export default defineComponent({
         }
       }
     }
+
+    .left.single {
+      ::v-deep(input) {
+        width: auto;
+      }
+    }
   }
 
   .calendar {
@@ -826,6 +842,7 @@ export default defineComponent({
         visibility: hidden;
       }
     }
+
     .right {
       margin-left: 40px;
       ::v-deep(.calendar__controls-left) {
