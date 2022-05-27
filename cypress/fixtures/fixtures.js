b0ab94f2b7f355109ccdefc279d158687bf5762b
novickay <novickaya@sputnikfund.ru>
2022-05-27 13:47:28 +0700

    complete calendar tests

--- cypress/fixtures/fixtures.js
+++ cypress/fixtures/fixtures.js
@@ -26,7 +26,7 @@ const ESelectProps = {
 
 const ECalendarProps = {
   inputData: {
-    // inputData - common data for date & time inputs
+    // inputData = common data for date & time inputs
     size: 'md',
   },
   showInput: true,
@@ -37,7 +37,6 @@ const ECalendarProps = {
     isAMPM: true,
     label: 'Choose time',
   },
-  //save here ?
   date: {
     date_from: '',
     date_to: '',
