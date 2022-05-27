7049d939ad073cb2c06d167332b202fa022e0f41
novickay <novickaya@sputnikfund.ru>
2022-05-27 13:54:47 +0700

    clean

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,4 @@
-<template>
-  <div :style="{ width: '700px' }">
-    <Calendar
-      :data="calendarData"
-      :style-config="{
-        fontFamily: 'Raleway',
-        fontSize: '14px',
-        fontWeight: 'regular',
-        activeColor: '#0066FF',
-        activeBackgroundColor: '#E5F0FF',
-      }"
-      @update:dateValue="(v) => check(v)"
-      @onError:inputValue="(errorMsg) => handleError(errorMsg)"
-    ></Calendar>
-  </div>
-</template>
+<template></template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -21,41 +6,11 @@ import Calendar from '@/components/calendar/Calendar.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { Calendar },
+  components: {},
   data() {
-    return {
-      calendarData: {
-        isDouble: false,
-        isRange: false,
-        showInput: true,
-
-        locale: 'en-US',
-        timePicker: {
-          isAMPM: true,
-        },
-        inputData: {
-          size: 'md',
-          // showFilled: true,
-          validators: [this.required],
-        },
-
-        timeSelectData: {
-          showFilled: false,
-        },
-      },
-    }
+    return {}
   },
-  methods: {
-    required(value) {
-      return !value ? 'Обязательное поле' : ''
-    },
-    handleError(error) {
-      console.log(error)
-    },
-    check(v) {
-      console.log(v)
-    },
-  },
+  methods: {},
 })
 </script>
 
