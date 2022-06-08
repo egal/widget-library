87855c9a53de0cc2c79a783cc49c2d9a1f92f925
Yuliya Yushkova <kuroi.kasa1509@gmail.com>
2022-06-08 18:28:54 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template></template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'App',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {},
 })
 </script>
 
 <style lang="scss">
-@import 'src/assets/variables';
-.wrapper {
-  margin-bottom: 30px;
-  width: 375px;
+.a {
+  display: grid;
+  grid-row-gap: 30px;
 }
 </style>
