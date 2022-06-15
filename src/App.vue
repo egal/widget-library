a62e58a4b6e6b2d53d80b9a0fff186dfb3a9a761
novickay <novickaya@sputnikfund.ru>
2022-06-15 16:17:03 +0700

    pagination

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
-<template></template>
+<template>
+  <div>
+    <EPagination :data="{ ...pData }"></EPagination>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import EPagination from '@/components/navigation/EPagination.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { EPagination },
   data() {
-    return {}
+    return {
+      // numberOfPages	Общее количество страниц	number	0
+      // modelValue	Текущая страница, внутреннее название во vue, в компонентах нужно использоват v-model	number	0
+      // leftArrowLabel?	Текст рядом с левой стрелкой	string
+      // rightArrowLabel?	Текст рядом с правой стрелкой	string
+      // size?	Размер	"sm", "md", "lg"	md
+      // componentStyle?	Стиль компонента	"normal", "light"	normal
+      // font?	Шрифт	"Inter", "Open Sans", "Raleway"	Open Sans
+      // weight?	Толщина шрифта	"medium", "regular", "bold"	regular
+      // color?	Цвет шрифта (обычный)	string
+      // activeColor?	Цвет шрифта (активный)	string
+      // activeBackgroundColor?	Цвет заднего плана (активный)	string
+      // borderColor?	Цвет рамки	string
+      // perPage	Количество записей на страницу	number	10
+      // dropdownPosition	Установка позиционирования списка опций	bottom, top	bottom
+      // selectOptions
+      pData: {},
+    }
   },
   mounted() {},
   methods: {},
